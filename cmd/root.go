c6852caf51aca337ed1a0f397f194ffc0f623743
kurtisvg <31518063+kurtisvg@users.noreply.github.com>
2026-06-08 04:33:41 -0600

    refactor: inline http flag defaults

--- cmd/root.go
+++ cmd/root.go
@@ -15,11 +15,6 @@ import (
 	flag "github.com/spf13/pflag"
 )
 
-const (
-	defaultHost = "127.0.0.1"
-	defaultPort = "8080"
-)
-
 type options struct {
 	host    string
 	port    string
@@ -29,8 +24,8 @@ type options struct {
 func parseFlags(args []string) (options, error) {
 	var opts options
 	fs := flag.NewFlagSet("ahh", flag.ContinueOnError)
-	fs.StringVar(&opts.host, "host", defaultHost, "HTTP listen host")
-	fs.StringVar(&opts.port, "port", defaultPort, "HTTP listen port")
+	fs.StringVar(&opts.host, "host", "127.0.0.1", "HTTP listen host")
+	fs.StringVar(&opts.port, "port", "8080", "HTTP listen port")
 	fs.BoolVar(&opts.version, "version", false, "Print version and exit")
 	err := fs.Parse(args)
 	return opts, err
